a31283368f034d9618609c4984379438d43c1e68
Charlie Greenman <charie@razroo.com>
2016-07-12 23:40:54 -0400

    Add eslintignore to brace for future jsx for cycle. Add neccesary plugins. Add base timer stream.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-require("../scss/core.scss");
+import xs from 'xstream';
+import {run} from '@cycle/xstream-run';
+import {makeDOMDriver, h1} from '@cycle/dom';
+
+function main() {
+  const sinks = {
+    DOM: xs.periodic(1000).map(i =>
+      h1('' + i + ' seconds elapsed')
+    )
+  };
+  return sinks;
+}
+
+const drivers = {
+  DOM: makeDOMDriver('#drawing_container')
+};
+
+run(main, drivers);
